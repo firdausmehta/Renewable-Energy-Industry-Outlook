89d462478adcfd798491fb2be242749ed72c1d12
Firdaus Mehta <firmehta97@gmail.com>
2021-09-29 19:20:00 +1000

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -4,3 +4,10 @@ var mapboxAccessToken = API_KEY;
 var map = L.map("map", {
     // layers: one_year
 }).setView([37.8, -96], 4);
+
+// create a function to resize the map for small screens
+reZoomMap(); 
+
+window.addEventListener("resize", function() {
+    reZoomMap();
+});
