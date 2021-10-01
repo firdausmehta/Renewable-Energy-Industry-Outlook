44424d224baa3b91dd58f10e70bb8f59ec9c0020
Firdaus Mehta <firmehta97@gmail.com>
2021-10-01 14:06:04 +1000

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,3 +5,8 @@ var url = "/api/us_energy";
 //  use d3 to get the data
 d3.json(url, function(data){
     
+    // grab values
+    var year = data.map(item => item.year)
+    var renewable_production = data.map(item => item.produced_renewable)
+    var total_consumed = data.map(item => item.total_consumed)
+    
