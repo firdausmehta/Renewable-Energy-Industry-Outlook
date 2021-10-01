632c94de79538861405da29d70587fe7398ac728
Firdaus Mehta <firmehta97@gmail.com>
2021-10-01 14:52:26 +1000

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,4 +9,14 @@ d3.json(url, function(data){
     var year = data.map(item => item.year)
     var renewable_production = data.map(item => item.produced_renewable)
     var total_consumed = data.map(item => item.total_consumed)
+
+    // create traces for renewable and total consumption 
+    var trace1 ={
+        x: year,
+        y: renewable_production,
+        type: "bar",
+        name: "Renewable Production",
+        marker: {color: 'rgb(26, 118, 255)'},
+        
+    };
     
